859f09411d3ae72f81dc36da28a4d53660170359
dicher74 <dicher74@gmail.com>
2024-10-21 19:38:32 +0300

    Added main components structure

---
+++ store-page/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="content-wrapper">
+		<PageBreadcrumbs />
+		<PageHeader />
+		<StoreFilters />
+		<StoreProductCards />
+	</div>
+</template>
+
+<style scoped>
+.content-wrapper {
+	display: inline-flex;
+	flex-direction: column;
+}
+</style>
